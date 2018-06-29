77541446b5ae8c7715bd24784ae5e9278017e146
Andrew Delapaz <ardelapaz@yahoo.com>
2018-06-28 22:28:22 -0400

    Progress made on project

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,9 +3,15 @@ require "httparty"
 
 module Kele
   include HTTParty
-  # Your code goes here...
+  
+  base_uri "https://www.bloc.io/api/v1/"
+
   def initialize(email, password)
-    @auth = { username: email, password: password }
+    response = self.class.post(api_url("sessions"), body: { username: email, password: password })
+    raise 'Invalid email or password, please try again.' if response.code == 404
+    @auth_token = response["auth token"]
+    puts response
+    puts @auth_token
   end
 
 end
